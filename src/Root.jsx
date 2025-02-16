e9e7ecfba0bc23dd0075f52b9456a977549f8e44
Shahriar Fardows <shahriarfardows@gmail.com>
2025-02-17 01:00:54 +0600

    navbar add

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,8 +1,12 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "./Shared/Navbar";
 
 const Root = () => {
     return (
-        <div>
+        <div className="relative">
+            <div className="sticky top-0 z-50">
+                <Navbar/>
+            </div>
             <Outlet/>
         </div>
     );
