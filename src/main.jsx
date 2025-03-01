1b9425f591fa11a1a738119d2ed1767b76380a0e
Shahriar Fardows <shahriarfardows@gmail.com>
2025-03-02 02:47:45 +0600

    all code update

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import {RouterProvider} from "react-router-dom";
-import "./index.css";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
+import './index.css';
 import Routers from './Routers/Routers.jsx';
 import Context from './Auth/Context/Context.jsx';
-import { Helmet } from 'react-helmet';
-
-
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-     <Helmet>
-     <Context>
-    <RouterProvider router={Routers} />
-      
+    <Context>
+      <RouterProvider router={Routers} />
     </Context>
-    </Helmet>
-    
-  </StrictMode>,
-)
+  </StrictMode>
+);
